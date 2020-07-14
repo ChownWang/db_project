5cef1f8644494a81c3323ec993be358679164d5f
Chown Wang <jie_1874@163.com>
2020-07-14 16:45:39 +0800

    do update

--- src/main/java/com/cj/controller/StartController.java
+++ src/main/java/com/cj/controller/StartController.java
@@ -1,6 +1,7 @@
 package com.cj.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,30 +9,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Description TODO
  * @Author ChownWang
  * @Date 2020/7/10 9:20
- * @Version 1.0lok,.
+ * @Version 1.0lok, .
  */
 @Controller
 public class StartController {
 
     @RequestMapping("/")
-    public String startIndex(){
+    public String startIndex() {
 
         return "starter";
     }
-    @RequestMapping("log/log_list")
+    /* @RequestMapping("log/log_list")
     public String doLogUI_logList() {
         return "sys/log_list";
     }
     @RequestMapping("menu/menu_list")
     public String doLoadUI_menuList(){
         return "sys/menu_list";
+    } */
+
+    @RequestMapping("{module}/{moduleUI}")
+    public String doLoadUI(@PathVariable(value = "moduleUI") String url, @PathVariable("module") String module) {
+        return "sys/" + url;
     }
 
     @RequestMapping("doPageUI")
     public String doPageUI() {
         return "common/page";
     }
-
 
 
 }
